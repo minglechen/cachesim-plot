e97440896417cc22e488259796ba9bc9b1829eb5
minglechen <minglechen2002@gmail.com>
2022-07-28 00:39:31 +0100

    Add output dir option

--- analyze_trace.py
+++ analyze_trace.py
@@ -10,21 +10,29 @@ outdir_dict = {}
 parser = argparse.ArgumentParser(description='Run drcachesim on trace with different cache sizes.')
 parser.add_argument('indirs', nargs='+',
                     help='input directories containing traces')
-parser.add_argument('--LL_cache_sizes', nargs='+', dest='LL_cache_sizes',
+parser.add_argument('-o', '--outdirs', nargs='+', help='output directories')
+parser.add_argument('--LL-cache-sizes', nargs='+', dest='LL_cache_sizes',
                     default=LL_cache_sizes,
                     help='specify LL cache sizes (default: [2M, 4M, 8M, 16M, 32M])')
-parser.add_argument('--drrun_bin', dest='bin', default='dynamorio/bin64/drrun', help='path to drrun binary')
-parser.add_argument('--remove_cached_results', dest='remove_cached_results', action='store_true', help='remove cached results')
-parser.add_argument('--rerun_sim', dest='rerun_sim', action='store_true', help='Rerun simulation')
+parser.add_argument('--drrun-bin', dest='bin', default='dynamorio/bin64/drrun', help='path to drrun binary')
+parser.add_argument('--remove-saved-results', dest='remove_saved_results', action='store_true', help='remove saved results')
+parser.add_argument('--rerun-sim', dest='rerun_sim', action='store_true', help='Rerun simulation')
 args = parser.parse_args()
 
-for indir in args.indirs:
-    base = os.path.basename(indir)
-    if base == "":
-        base  = os.path.basename(os.path.split(indir)[0])
-    outdir = base + '_results'
+if args.outdirs is not None and len(args.outdirs) != len(args.indirs):
+    print('Number of output directories must match number of input directories')
+    exit(1)
+
+for i, indir in enumerate(args.indirs):
+    if args.outdirs is None:
+        base = os.path.basename(indir)
+        if base == "":
+            base  = os.path.basename(os.path.split(indir)[0])
+        outdir = base + '_results'
+    else:
+        outdir = args.outdirs[i]
     outdir_dict[indir] = outdir
-    if args.remove_cached_results:
+    if args.remove_saved_results:
         shutil.rmtree(outdir, ignore_errors=True)
     if not os.path.exists(outdir):
         os.makedirs(outdir)
